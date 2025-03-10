9e09b7664c3c98feda31834793f74eea1811c98c
Sushim <sushim.pingale05@gmail.com>
2025-03-10 17:30:44 +0530

    10th March - Exercise 10

---
+++ src/ex_01_Java_Basic/Lab009_Keywords_Identifier.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab009_Keywords_Identifier {
+    public static void main(String[] args) {
+        System.out.println("Hi, Keyword");
+//        Keywords (blue colour) : - int, class, public, static, void, if, else, while,for
+
+    }
+}
