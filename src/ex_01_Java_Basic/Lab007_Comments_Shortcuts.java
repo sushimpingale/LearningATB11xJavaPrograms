8a1f6dfd5b3d01f9a83dd9c19ac1113b4335510c
Sushim <sushim.pingale05@gmail.com>
2025-03-10 17:23:51 +0530

    10th March - Exercise 08

---
+++ src/ex_01_Java_Basic/Lab007_Comments_Shortcuts.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Comments_Shortcuts {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+//    Shortcuts : ctrl+D using duplicate
+}
