47febccb8fbb15039ebda6fb09e21d17162cd411
Sushim <sushim.pingale05@gmail.com>
2025-03-10 17:52:42 +0530

    10th March - Exercise 14

---
+++ src/ex_01_Java_Basic/Lab013_println_vs_Print.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab013_println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+    }
+}
+//        Println : use for print to next line
+//        Print : use for print to same line
