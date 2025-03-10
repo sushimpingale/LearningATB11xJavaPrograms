588e7a3782e082780477b5403323d9e380b4f7f7
Sushim <sushim.pingale05@gmail.com>
2025-03-10 17:15:39 +0530

    10th March - Exercise 05

---
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi");
+    }
+}
+// This is comment, it will be ignored by the compiler using ctrl+/
