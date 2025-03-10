34d7662b8e7a99daed2f6b632b205a963622df42
Sushim <sushim.pingale05@gmail.com>
2025-03-10 17:37:04 +0530

    10th March - Exercise 11

---
+++ src/ex_01_Java_Basic/Lab010_Keywords_Identifier.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab010_Keywords_Identifier {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+//        Identifier : - score, highestScore, convertToString, number1
+//        In the above Java code, we have 5 identifiers as follows:
+//        Test: Class Name.
+//        main: Method Name.
+//        String: Predefined Class Name.
+//                args: Variable Name.
+//        a: Variable Name.
+
+    }
+}
