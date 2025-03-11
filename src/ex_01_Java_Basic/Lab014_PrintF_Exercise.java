b1f635e21b560414a48252d2e91b0bd8b79fdeeb
Sushim <sushim.pingale05@gmail.com>
2025-03-11 21:48:49 +0530

    11th March - PrintF Exercise

---
+++ src/ex_01_Java_Basic/Lab014_PrintF_Exercise.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab014_PrintF_Exercise {
+    public static void main(String[] args) {
+        int table =9;
+        System.out.printf("%d*1=9",table);
+        System.out.println("");
+        System.out.printf("%d*2=18",table);
+        System.out.println("");
+        System.out.printf("%d*10=90",table);
+
+    }
+}
